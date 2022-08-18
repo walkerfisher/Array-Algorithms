82a64036ce481dc620c69fcd4c96c0d2991ea500
walkerfisher <101424637+walkerfisher@users.noreply.github.com>
2022-08-18 10:33:23 -0700

    TwoNumberSum complete

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Array-Algorithms
-Library of problem sets for algorithmic logic
+
+Library of problem sets for algorithmic logic regarding Arrays.
+
+All problem sets each have a folder in the explorer.
+
+Problem sets are sourced from LeetCode & AlgoExpert for Coding Interview practice.
---
+++ TwoNumberSum.js
@@ -0,0 +1,19 @@
+function twoNumberSum(array, targetSum) {
+  const numbers = {};
+
+  for (const number of array) {
+    const potentialMatch = targetSum - number;
+    if (potentialMatch in numbers) {
+      return [potentialMatch, number];
+    } else {
+      numbers[number] = true;
+    }
+  }
+  return [];
+}
+
+//TEST CASES//
+console.log(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10));
+console.log(twoNumberSum([4, 6], 10));
+console.log(twoNumberSum([-21, 301, 12, 4, 65, 56, 210, 356, 9, -47], 163));
+//----------//
